88449e48ff6e4f6a9c6da8c91bb6d2dd2ffff325
BrickWasTaken <none>
2022-11-17 17:08:24 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from random import randint
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class YellowCircles(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.to_repaint = False
+
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.btn_click)
+        self.setFixedSize(600, 600)
+
+    def btn_click(self):
+        self.to_repaint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.to_repaint:
+            self.draw_circle(QPainter(self))
+
+    @staticmethod
+    def draw_circle(qp):
+        qp.setBrush(QColor(255, 255, 0))
+
+        s = randint(10, 100)
+        x, y = randint(0, 600 - s), randint(10, 600 - s)
+        qp.drawEllipse(x, y, s, s)
+
+
+def except_hook(c, e, t):
+    sys.__excepthook__(c, e, t)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = YellowCircles()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
